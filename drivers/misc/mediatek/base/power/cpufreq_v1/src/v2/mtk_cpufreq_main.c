f629fcff83901dd035dda69a17d163030c334129
claxten10 <claxten10@gmail.com>
2024-10-01 00:34:02 +0000

    Revert "[ALPS07031095] CPU DVFS: Remove governor per policy for mt6781"
    * Adds back support for governor per policy for MT6781

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/v2/mtk_cpufreq_main.c
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/v2/mtk_cpufreq_main.c
@@ -1525,7 +1525,7 @@ static struct freq_attr *_mt_cpufreq_attr[] = {
 static struct cpufreq_driver _mt_cpufreq_driver = {
 #ifndef CONFIG_MTK_CPU_FREQ_STANDARDIZE
 #if defined(CONFIG_MTK_PLAT_MT6885_EMULATION) || defined(CONFIG_MACH_MT6893) \
-	|| defined(CONFIG_MACH_MT6833)
+	|| defined(CONFIG_MACH_MT6833) || defined(CONFIG_MACH_MT6781)
 	.flags = CPUFREQ_ASYNC_NOTIFICATION | CPUFREQ_HAVE_GOVERNOR_PER_POLICY,
 #else /* CONFIG_MTK_PLAT_MT6885_EMULATION */
 	.flags = CPUFREQ_ASYNC_NOTIFICATION,
